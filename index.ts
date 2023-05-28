157a80905ffb0cf8fdac67e955b4d1734922a4d1
ayush <ayush.chauhan0008@gmail.com>
2023-05-28 23:49:18 +0530

    bmi calculator server

--- index.ts
+++ index.ts
@@ -3,7 +3,24 @@ const app = express()
 
 // underscore before variable to ignore warnings
 app.get("/", (_req, res) => {
-	res.send('welcome')
+	res.send("welcome")
+})
+
+app.get("/hello", (_req, res) => {
+	res.send("Hello Full Stack")
+})
+
+app.get("/bmi", (req, res) => {
+	const params = req.query
+	let { height, weight } = params
+	let heightToMeters: number = Number(height) / 100
+	let weightToKgs: number = Number(weight)
+	let bmi: number = weightToKgs / (heightToMeters * heightToMeters)
+	res.send({
+		height: heightToMeters,
+		weight: weightToKgs,
+		bmi: bmi,
+	})
 })
 
 const PORT = 3000
