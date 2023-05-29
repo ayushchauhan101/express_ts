bba669293193c468174c54158937cf6b3b4f896c
ayush <ayush.chauhan0008@gmail.com>
2023-05-29 14:09:48 +0530

    calculator endpoint incomplete

--- calculator.ts
+++ calculator.ts
@@ -1,17 +1,19 @@
-type Operation = "add" | "substract" | "multiply" | "divide"
+export type Operation = "add" | "substract" | "multiply" | "divide";
 
-function calculator(a: number, b: number, op: Operation): number | string {
+export function calculator(
+	a: number,
+	b: number,
+	op: Operation
+): number | string {
 	if (isNaN(a) || isNaN(b)) {
-		return "given input is not a number"
+		return "given input is not a number";
 	} else {
-		if (op === "add") return a + b
-		else if (op === "substract") return a - b
-		else if (op === "multiply") return a * b
+		if (op === "add") return a + b;
+		else if (op === "substract") return a - b;
+		else if (op === "multiply") return a * b;
 		else if (op === "divide") {
-			if (a === 0 || b === 0) return "Cannot divide by 0"
-			else return a / b
-		} else return "Wrong data type"
+			if (a === 0 || b === 0) return "Cannot divide by 0";
+			else return a / b;
+		} else return "Wrong data type";
 	}
 }
-
-export default calculator
--- index.ts
+++ index.ts
@@ -1,21 +1,35 @@
-import express from "express"
-import calculator from "./calculator"
+import express from "express";
+import { calculator, Operation } from "./calculator";
 
-const app = express()
+const app = express();
 
 // underscore before variable to ignore warnings
 app.get("/", (_req, res) => {
-	res.send("welcome")
-})
+	res.send("welcome");
+});
 
 app.post("/calculate", (req, res) => {
-	const { num1, num2, op } = req.body
-	const result = calculator(num1, num2, op)
-	res.send({ result })
-})
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const { num1, num2, op } = req.body;
 
-const PORT = 3000
+	if (!num1 || isNaN(Number(num1))) {
+		return res
+			.status(400)
+			.send({ error: num1 + "sent value is not a valid number" });
+	} else if (!num2 || isNaN(Number(num2))) {
+		return res
+			.status(400)
+			.send({ error: num1 + "sent value is not a valid number" });
+	} else {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+		const result = calculator(Number(num1), Number(num2), op as Operation);
+
+		return res.send({ result });
+	}
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
-	console.log(`Server running on PORT ${PORT}`)
-})
+	console.log(`Server running on PORT ${PORT}`);
+});
